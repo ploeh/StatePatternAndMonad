25a2cab93922e96eedfcdbf18f4322d718cb14b8
Mark Seemann <mark@ploeh.dk>
2021-07-15 16:46:06 +0200

    Add some multi-use examples

--- State/Examples.cs
+++ State/Examples.cs
@@ -88,5 +88,34 @@ namespace Ploeh.Samples.StatePattern
             Assert.Equal(Out2.Theta, actual);
             Assert.Equal(new ConcreteStateB(), ctx.State);
         }
+
+        [Fact]
+        public void MultiUseExample1()
+        {
+            var in1 = In1.Alpha;
+            var ctx = new Context(new ConcreteStateA());
+
+            var outA = ctx.Request1(in1);
+            var outB = ctx.Request1(in1);
+
+            Assert.Equal(Out1.Gamma, outA);
+            Assert.Equal(Out1.Gamma, outB);
+            Assert.Equal(new ConcreteStateA(), ctx.State);
+        }
+
+        [Fact]
+        public void MultiUseExample2()
+        {
+            var in2 = In2.Epsilon;
+            var in1 = In1.Beta;
+            var ctx = new Context(new ConcreteStateA());
+
+            var outA = ctx.Request2(in2);
+            var outB = ctx.Request1(in1);
+
+            Assert.Equal(Out2.Eta, outA);
+            Assert.Equal(Out1.Gamma, outB);
+            Assert.Equal(new ConcreteStateA(), ctx.State);
+        }
     }
 }
