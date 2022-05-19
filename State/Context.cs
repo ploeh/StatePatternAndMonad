3ad990ea0d56ccabb71b4e2df0b3b04220bf4da1
Mark Seemann <mark@ploeh.dk>
2022-05-19 12:53:35 +0200

    Delete Request1 method
    It was no longer in use.

--- State/Context.cs
+++ State/Context.cs
@@ -16,11 +16,6 @@ namespace Ploeh.Samples.StatePattern
         public State State { get; }
 
         // Generalised methods
-        public StatePair<Context, Out1> Request1(In1 in1)
-        {
-            return in1.Request1().Run(this);
-        }
-
         public StatePair<Context, Out2> Request2(In2 in2)
         {
             return in2.Request2().Run(this);
