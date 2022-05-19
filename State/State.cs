c36257e38fa526a51babb8c6c3cc5a3c19a48fb6
Mark Seemann <mark@ploeh.dk>
2022-05-19 12:45:18 +0200

    Remove State.Handle
    The motivation is the same as explained in the previous commit message.

--- State/State.cs
+++ State/State.cs
@@ -2,12 +2,6 @@
 {
     public abstract class State
     {
-        public virtual State Handle(Context context)
-        {
-            return this;
-        }
-
-        // Generalised methods
         public abstract StatePair<State, Out1> Handle1(In1 in1);
 
         public abstract StatePair<State, Out2> Handle2(In2 in2);
